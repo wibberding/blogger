7c485f233f83c84369372ad55a7f68c89ca8ea17
Lonnie <wibberding@gmail.com>
2015-05-26 15:21:31 -0700

    I5 done

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   end
   root to: "articles#index"
   resources :tags
+  resources :authors
+
+  resources :author_sessions, only: [:new, :create, :destroy ]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
